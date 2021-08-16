f8603a0e963b4a30ad7cebfa6a4dc19612e670aa
yamashita623 <623msk@gmail.com>
2021-08-16 22:02:09 +0900

    merge

--- src/views/First.vue
+++ src/views/First.vue
@@ -12,8 +12,7 @@
     <br />
 
     <label for="birthday"
-      >生年月日:
-      <input type="date" id="birthday" v-model="birthday" /> </label
+      >生年月日: <input type="date" id="birthday" v-model="birthday" /> </label
     ><br />
 
     <button><router-link to="/Second">次に進む</router-link></button>
@@ -27,7 +26,7 @@ export default {
         return this.$store.getters.getGender;
       },
       set(val) {
-        this.$store.dispatch("setGender", val);
+        this.$store.dispatch('setGender', val);
       },
     },
     birthday: {
@@ -35,7 +34,7 @@ export default {
         return this.$store.getters.getBirthday;
       },
       set(val) {
-        this.$store.dispatch("setBirthday", val);
+        this.$store.dispatch('setBirthday', val);
       },
     },
   },
